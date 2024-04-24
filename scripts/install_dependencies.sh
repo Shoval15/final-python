20d4fda4c0314ba392212fb248d5b9699e5135cd
Shoval15 <127046404+Shoval15@users.noreply.github.com>
2024-04-24 23:54:42 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# # update the instance
-# sudo yum update -y
-# # install docker
-# sudo yum install docker -y
-# # start and enable the Docker service
-# sudo systemctl start docker
-# sudo systemctl enable docker
+# update the instance
+sudo yum update -y
+# install docker
+sudo yum install docker -y
+# start the Docker service
+sudo service docker start
 
-# # execute docker commands without using sudo
-# sudo usermod -a -G docker ec2-user
-# newgrp docker
+# execute docker commands without using sudo
+sudo usermod -a -G docker ec2-user
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh
+# Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
