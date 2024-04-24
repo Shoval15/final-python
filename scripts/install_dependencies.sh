10b210c59c8fc2cce2258445d84120fc184ea994
Shoval15 <127046404+Shoval15@users.noreply.github.com>
2024-04-25 00:02:00 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,7 +8,3 @@ sudo service docker start
 
 # execute docker commands without using sudo
 sudo usermod -a -G docker ec2-user
-
-# Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
