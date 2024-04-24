eff41c14f897cc8247a56e441f35811173d69b95
Shoval15 <127046404+Shoval15@users.noreply.github.com>
2024-04-24 23:38:49 +0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # update the instance
-sudo yum update
+sudo yum update -y
 # install docker
-sudo yum install docker
+sudo yum install docker -y
+# start and enable the Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
 # execute docker commands without using sudo
 sudo usermod -a -G docker ec2-user
-id ec2-user
 newgrp docker
